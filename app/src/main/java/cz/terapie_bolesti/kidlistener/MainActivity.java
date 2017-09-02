6d95690a76477286499ec30e13734fe50d36dfa0
Amir Zolal <amirzolal@gmail.com>
2017-09-02 21:14:44 +0200

    5 results analyzed

--- app/src/main/java/cz/terapie_bolesti/kidlistener/MainActivity.java
+++ app/src/main/java/cz/terapie_bolesti/kidlistener/MainActivity.java
@@ -2,6 +2,10 @@ package cz.terapie_bolesti.kidlistener;
 
 import java.util.ArrayList;
 import java.util.Locale;
+
+import android.annotation.TargetApi;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -23,16 +27,38 @@ public class MainActivity extends AppCompatActivity {
         txtViewer = (TextView) findViewById(R.id.txtViewer);
     }
 
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     public void onButtonClick(View v) {
         Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
-                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "cs_CZ");
         intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Mluv");
+        intent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, 5);
         try {
             startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
         } catch (ActivityNotFoundException a) {
             txtViewer.setText("Nefunguje!");
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case REQ_CODE_SPEECH_INPUT: {
+                if (resultCode == RESULT_OK && null != data) {
+
+                    ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    txtViewer.setText("");
+                    for (int a=0;a<result.size();a++) {
+                        txtViewer.append(result.get(a)+'\n');
+                    }
+                }
+                break;
+            }
+
         }
     }
 
